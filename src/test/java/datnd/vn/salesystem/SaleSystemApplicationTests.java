a790d4fbed0ff193fabbf166ec95063004a68f8b
dat.nd <dat.nd@lpbs.com.vn>
2026-04-13 14:37:27 +0700

    first commit

---
+++ src/test/java/datnd/vn/salesystem/SaleSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package datnd.vn.salesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
